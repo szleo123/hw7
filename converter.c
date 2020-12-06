52107172a7d5891a080689c12d2a63663840ebd7
szleo123 <yilli@reed.edu>
2020-12-06 13:39:12 -0800

    part1

--- converter.c
+++ converter.c
@@ -5,10 +5,24 @@
 #include "converter.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 void convert_all(unsigned nlines, char *lines[], quote_t nums[])
 {
   for (unsigned i = 0; i < nlines; i++) {
-    nums[i] = atoi(lines[i]);
+    char* cur = lines[i];
+    // find the size of the quote
+    char* cursize = cur;
+    size_t size = 0;
+    while(*cursize++) ++size;
+
+    uint32_t result = 0; 
+    int timer = 1;
+    for (int i = size-1; i >=0; --i){
+    	int dec = cur[i] - '0';
+    	result += (timer * dec);
+    	timer *= 10; 
+    }
+    nums[i] = result;
   }
 }
